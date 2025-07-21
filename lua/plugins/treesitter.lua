03c8ee8b53299a229740f982026c89a13f05bda7
Moeiz Riaz <moeizriaz@gmail.com>
2025-07-21 10:03:24 -0400

    more colors

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
   lazy = false,
   branch = 'main',
   build = ":TSUpdate",
+  enabled = true,
   config = function () 
      require('nvim-treesitter').install{"c", "cpp", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html"}
   end
